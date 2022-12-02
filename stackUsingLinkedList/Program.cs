6a38c8e5ea31d4871d40d06d7706f6e3d0b3044e
wildmove <muhammad.wildan,ft21@mail.umy.ac.id>
2022-12-02 16:51:56 +0700

    process the menu with switch case

--- stackUsingLinkedList/Program.cs
+++ stackUsingLinkedList/Program.cs
@@ -76,7 +76,31 @@ namespace StackUsingLinkedList_CSharp
                 Console.WriteLine("4. Exit");
                 Console.Write("\nEnter your choice: ");
                 string sInput = Console.ReadLine();
-                
+                char ch = Convert.ToChar(sInput == "" ? "0" : sInput);
+                switch (ch)
+                {
+                    case '1':
+                        Console.Write("\nEnter a number: ");
+                        int num = Convert.ToInt32(Console.ReadLine());
+                        s.push(num);
+                        break;
+                    case '2':
+                        if (s.empty())
+                        {
+                            Console.WriteLine("\nStack Empty");
+                            break;
+                        }
+                        s.pop();
+                        break;
+                    case '3':
+                        s.display();
+                        break;
+                    case '4':
+                        return;
+                    default:
+                        Console.WriteLine("\nInvalid Choice");
+                        break;
+                }
 
             }
         }
