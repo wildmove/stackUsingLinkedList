9a7e66af9e4acc57dd7fdf25ca714f67b426bacc
wildmove <muhammad.wildan,ft21@mail.umy.ac.id>
2022-12-02 16:26:29 +0700

    create Node class

--- stackUsingLinkedList/Program.cs
+++ stackUsingLinkedList/Program.cs
@@ -2,5 +2,14 @@
 
 namespace StackUsingLinkedList_CSharp
 {
-
+    class Node
+    {
+        public int info;
+        public Node next;
+        public Node(int i, Node n)
+        {
+            info = i;
+            next = n;
+        }
+    }
 }
