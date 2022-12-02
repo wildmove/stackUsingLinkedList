8c14714d8ad86fdbddba0d97ddedb95e3447e2f1
wildmove <muhammad.wildan,ft21@mail.umy.ac.id>
2022-12-02 16:34:17 +0700

    add method push

--- stackUsingLinkedList/Program.cs
+++ stackUsingLinkedList/Program.cs
@@ -31,6 +31,16 @@ namespace StackUsingLinkedList_CSharp
                 return (false);
         }
 
+        public void push(int element)
+        {
+            Node fresh;
+            fresh = new Node(element, null);
+
+            fresh.next = top;
+            top = fresh;
+            Console.WriteLine("\n" + element + " pushed. ");
+        }
+
         
 
 
