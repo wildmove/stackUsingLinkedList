66a6abe3d2503327eed9c324eb7d01b91833d91b
wildmove <muhammad.wildan,ft21@mail.umy.ac.id>
2022-12-02 16:51:31 +0700

    add menu inside method Main()

--- stackUsingLinkedList/Program.cs
+++ stackUsingLinkedList/Program.cs
@@ -63,6 +63,24 @@ namespace StackUsingLinkedList_CSharp
             }
         }
 
+        static void Main(string[] args)
+        {
+            Stacks s = new Stacks();
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("\n***Stack Menu***\n");
+                Console.WriteLine("1. Push.");
+                Console.WriteLine("2. Pop. ");
+                Console.WriteLine("3. Display. ");
+                Console.WriteLine("4. Exit");
+                Console.Write("\nEnter your choice: ");
+                string sInput = Console.ReadLine();
+                
+
+            }
+        }
+
         
 
 
